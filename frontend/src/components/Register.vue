d50f7b73efea1f28178206399e57cae6905b2578
Nathalia Cristina Ribeiro Massaia <nathalia.massaia@pampa.compasso>
2021-07-18 17:42:06 -0400

    create-register-page

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -1,81 +1,55 @@
 <template>
-  <div class='registerWrapper'>
-    <h1>Register</h1>
-<v-card>
-  <v-form
-    ref="form"
-    v-model="valid"
-    lazy-validation
-  >
-    <v-text-field
-      v-model="name"
-      :counter="10"
-      :rules="nameRules"
-      label="Name"
-      required
-    ></v-text-field>
+  <div class="registerWrapper">
+    <h3>Cadastro de cliente</h3>
+    <v-card>
+      <v-form ref="form" v-model="valid" lazy-validation>
+        <v-text-field
+          v-model="name"
+          :counter="10"
+          :rules="nameRules"
+          label="Nome"
+          required
+        ></v-text-field>
 
-    <v-text-field
-      v-model="email"
-      :rules="emailRules"
-      label="E-mail"
-      required
-    ></v-text-field>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="Endereço"
+          required
+        ></v-text-field>
+        <v-text-field
+          v-model="email"
+          :rules="emailRules"
+          label="Número de telefone"
+          required
+        ></v-text-field>
 
-    <v-select
-      v-model="select"
-      :items="items"
-      :rules="[v => !!v || 'Item is required']"
-      label="Item"
-      required
-    ></v-select>
+        <v-btn
+          :disabled="!valid"
+          color="success"
+          class="mr-4"
+          @click="confirme"
+        >
+          Confirmar
+        </v-btn>
 
-    <v-checkbox
-      v-model="checkbox"
-      :rules="[v => !!v || 'You must agree to continue!']"
-      label="Do you agree?"
-      required
-    ></v-checkbox>
-
-    <v-btn
-      :disabled="!valid"
-      color="success"
-      class="mr-4"
-      @click="validate"
-    >
-      Validate
-    </v-btn>
-
-    <v-btn
-      color="error"
-      class="mr-4"
-      @click="reset"
-    >
-      Reset Form
-    </v-btn>
-
-    <v-btn
-      color="warning"
-      @click="resetValidation"
-    >
-      Reset Validation
-    </v-btn>
-  </v-form>
-</v-card>
+        <v-btn color="error" class="mr-4" @click="reset"> Cancelar </v-btn>
+      </v-form>
+    </v-card>
   </div>
 </template>
 
 <script lang='ts'>
-import Vue from 'vue';
+import Vue from "vue";
 
 export default Vue.extend({
-  name: 'Register',
-  data(){
-    return {}
-  }
+  name: "Register",
+  data() {
+    return {};
+  },
 });
 </script>
 
 <style lang="scss">
-@import '@/sass/master';
+@import "@/sass/master";
 </style>
